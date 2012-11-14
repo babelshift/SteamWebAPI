4f96a6097d6838e0b62556e4fa7e067186339726
Justin Skiles <jskiles1@gmail.com>
2012-11-13 23:28:10 -0500

    Added rss/atom feed classes and methods to read Valve/Steam feeds

---
+++ SteamWebAPI.WinRT/SteamFeedRequest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SteamWebModel;
+using Windows.Web.Syndication;
+
+namespace SteamWebAPI
+{
+    public class SteamFeedRequest
+    {
+        protected string E_FEED_REQUEST_FAILED = "An error occurred while handling the feed request. Check the URI.";
+
+        public async Task<FeedData> GetFeedAsync(string uri)
+        {
+            Windows.Web.Syndication.SyndicationClient client = new SyndicationClient();
+            Uri feedUri = new Uri(uri);
+
+            try
+            {
+                SyndicationFeed feed = await client.RetrieveFeedAsync(feedUri);
+                FeedData feedData = DeserializeFeedData(feed);
+                return feedData;
+            }
+            catch
+            {
+                throw new Exception(E_FEED_REQUEST_FAILED);
+            }
+        }
+
+        private FeedData DeserializeFeedData(SyndicationFeed feed)
+        {
+            try
+            {
+                FeedData feedData = new FeedData();
+
+                if (feed.Title != null && feed.Title.Text != null)
+                {
+                    feedData.Title = feed.Title.Text;
+                }
+                if (feed.Subtitle != null && feed.Subtitle.Text != null)
+                {
+                    feedData.Description = feed.Subtitle.Text;
+                }
+                if (feed.Items != null && feed.Items.Count > 0)
+                {
+                    // Use the date of the latest post as the last updated date.
+                    feedData.PublishDate = feed.Items[0].PublishedDate.DateTime;
+
+                    foreach (SyndicationItem item in feed.Items)
+                    {
+                        FeedItem feedItem = new FeedItem();
+                        if (item.Title != null && item.Title.Text != null)
+                        {
+                            feedItem.Title = item.Title.Text;
+                        }
+                        if (item.PublishedDate != null)
+                        {
+                            feedItem.PublishDate = item.PublishedDate.DateTime;
+                        }
+                        if (item.Authors != null && item.Authors.Count > 0)
+                        {
+                            feedItem.Author = item.Authors[0].Name.ToString();
+                        }
+
+                        // Handle the differences between RSS and Atom feeds.
+                        if (feed.SourceFormat == SyndicationFormat.Atom10)
+                        {
+                            if (item.Content != null && item.Content.Text != null)
+                            {
+                                feedItem.Content = item.Content.Text;
+                            }
+                            if (item.Id != null)
+                            {
+                                feedItem.Link = new Uri(item.Id);
+                            }
+                        }
+                        else if (feed.SourceFormat == SyndicationFormat.Rss20)
+                        {
+                            if (item.Summary != null && item.Summary.Text != null)
+                            {
+                                feedItem.Content = item.Summary.Text;
+                            }
+                            if (item.Links != null && item.Links.Count > 0)
+                            {
+                                feedItem.Link = item.Links[0].Uri;
+                            }
+                        }
+                        feedData.Items.Add(feedItem);
+                    }
+                }
+                return feedData;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ SteamWebAPI.WinRT/SteamFeedSession.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SteamWebModel;
+using Windows.Web.Syndication;
+
+namespace SteamWebAPI
+{
+    public class SteamFeedSession
+    {
+        private SteamFeedRequest steamFeedRequest;
+
+        public enum FeedType
+        {
+            Dota2,
+            SteamWorkshop,
+            Left4Dead,
+            TeamFortress2,
+            Portal2,
+            CounterStrikeGo,
+            SourceFilmmaker,
+            RamblingsInValveTime,
+            ValveEconomics,
+            ValveLinuxTeamBlog,
+            TeachWithPortals,
+        }
+
+        public SteamFeedSession()
+        {
+            steamFeedRequest = new SteamFeedRequest();
+        }
+
+        public async Task<FeedData> GetFeedAsync(FeedType feed)
+        {
+            string feedUri = String.Empty;
+
+            if (feed == FeedType.Dota2)
+                feedUri = "http://blog.dota2.com/feed/";
+            else if (feed == FeedType.SteamWorkshop)
+                feedUri = "http://steamcommunity.com/games/SteamWorkshop/rss";
+            else if (feed == FeedType.Left4Dead)
+                feedUri = "http://www.l4d.com/blog/rss.xml";
+            else if (feed == FeedType.TeamFortress2)
+                feedUri = "http://tf2.com/rss.xml";
+            else if (feed == FeedType.Portal2)
+                feedUri = "http://www.thinkwithportals.com/rss.xml";
+            else if (feed == FeedType.SourceFilmmaker)
+                feedUri = "http://www.sourcefilmmaker.com/rss.xml";
+            else if (feed == FeedType.RamblingsInValveTime)
+                feedUri = "http://blogs.valvesoftware.com/feed/?cat=3";
+            else if (feed == FeedType.ValveEconomics)
+                feedUri = "http://blogs.valvesoftware.com/feed/?cat=6";
+            else if (feed == FeedType.ValveLinuxTeamBlog)
+                feedUri = "http://blogs.valvesoftware.com/feed/?cat=7";
+            else if (feed == FeedType.TeachWithPortals)
+                feedUri = "http://www.teachwithportals.com/index.php/feed/";
+            else if (feed == FeedType.CounterStrikeGo)
+                feedUri = "http://blog.counter-strike.net/index.php/feed/";
+
+            return await steamFeedRequest.GetFeedAsync(feedUri);
+        }
+    }
+}
